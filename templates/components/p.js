00c9e8f1edf5e8defc8afd75a1d7d7d2a10d7c77
qianmoQ <shicheng@devlive.org>
2024-12-30 12:10:50 +0800

    适配 TOC

--- templates/components/p.js
+++ templates/components/p.js
@@ -1,6 +1,6 @@
 module.exports = function h1(item) {
     return `
-        <p class="text-base text-gray-700 leading-7 mb-4">
+        <p class="text-base text-gray-700 leading-7">
             ${item.text}
         </p>
     `;
