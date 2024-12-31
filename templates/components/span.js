771a8a4ec75fef08680ea1462979a65c22233889
qianmoQ <shicheng@devlive.org>
2024-12-31 23:27:48 +0800

    支持列表项解析

--- templates/components/span.js
+++ templates/components/span.js
@@ -1,7 +1,5 @@
 module.exports = function h1(item) {
     return `
-        <span class="text-base text-gray-700 leading-7 my-2">
-            ${item.text}
-        </span>
+        <span class="text-base text-gray-700 leading-7 my-2">${item.text}</span>
     `;
 };
