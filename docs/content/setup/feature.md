b90e2dca882af08c1fc02df1c3bbe3314b6d4d1c
qianmoQ <shicheng@devlive.org>
2025-01-01 12:40:45 +0800

    添加显示国际化选择器

--- docs/content/setup/feature.md
+++ docs/content/setup/feature.md
@@ -26,3 +26,62 @@ lucide:
 
 - `enable`: 是否启用 Lucide 图标
 - `cdn`: Lucide CDN 链接，默认为 `https://unpkg.com/lucide@latest/dist/umd/lucide.js` ，可以根据自己的需求进行修改，这里要填写完整的 CDN 链接，例如 `https://unpkg.com/lucide@latest/dist/umd/lucide.js`
+
+## 国际化设置
+
+---
+
+PageForge 支持用户自定义的国际化设置，需要启用后才能使用国际化。
+
+```markdown
+i18n:
+    enable: true
+```
+
+启用国际化后，需要配置国际化语言
+
+```markdown
+i18n:
+  default: zh-CN
+  en:
+    name: English
+    flag: 🇬🇧
+    translations:
+      GetStarted: Getting Started
+      Setup: Setup
+      Usage: Usage Docs
+  zh-CN:
+    name: 中文
+    flag: 🇨🇳
+    translations:
+      GetStarted: 快速开始
+      Setup: 设置
+      Usage: 使用文档
+```
+
+- `default`: 默认语言
+- `en`、`zh-CN`: 自定义语言
+- `translations`: 对应语言的翻译，格式为 `key: value`
+
+系统会通过 `pageforge.yaml` 中配置的语言来自动加载对应的翻译，如果没有找到翻译，则会返回原 key
+
+在 `pageforge.yaml` 中配置国际化后，可以在 `nav` 中使用以下方式来获取翻译的结果。
+
+```markdown
+nav:
+- GetStarted:
+    - /getting-started/get-started
+```
+
+- `GetStarted`: 要翻译的 key，如果没有找到翻译，则会返回原 key
+
+在页面中使用使用以下方式来获取翻译的结果
+
+```markdown
+---
+title: GetStarted
+icon: crosshair
+---
+```
+
+只需要将 `title` 中替换为国际化的 key 就可以了
--- templates/assets/js/pageforge.js
+++ templates/assets/js/pageforge.js
@@ -45,10 +45,10 @@ const GitHubStats = {
 
         // 更新所有容器为加载状态
         ['.tag-container', '.stars-container', '.forks-container'].forEach(selector => {
-            const container = document.querySelector(selector);
-            if (container) {
+            const containers = document.querySelectorAll(selector);
+            containers.forEach(container => {
                 container.innerHTML = '<div class="skeleton-loader"></div>';
-            }
+            });
         });
     },
 
@@ -113,33 +113,34 @@ const GitHubStats = {
             return;
         }
 
-        const tagContainer = document.querySelector('.tag-container');
-        const starsContainer = document.querySelector('.stars-container');
-        const forksContainer = document.querySelector('.forks-container');
+        // 修改为更新所有匹配的容器
+        const tagContainers = document.querySelectorAll('.tag-container');
+        const starsContainers = document.querySelectorAll('.stars-container');
+        const forksContainers = document.querySelectorAll('.forks-container');
 
-        if (tagContainer) {
-            tagContainer.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
-                <path d="M1 7.775V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 0 1 0 2.474l-5.026 5.026a1.75 1.75 0 0 1-2.474 0l-6.25-6.25A1.75 1.75 0 0 1 1 7.775Zm1.5 0c0 .066.026.13.073.177l6.25 6.25a.25.25 0 0 0 .354 0l5.025-5.025a.25.25 0 0 0 0-.354l-6.25-6.25a.25.25 0 0 0-.177-.073H2.75a.25.25 0 0 0-.25.25ZM6 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2Z"></path>
-            </svg>
-            ${stats.tag}`;
-        }
+        tagContainers.forEach(container => {
+            container.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
+            <path d="M1 7.775V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 0 1 0 2.474l-5.026 5.026a1.75 1.75 0 0 1-2.474 0l-6.25-6.25A1.75 1.75 0 0 1 1 7.775Zm1.5 0c0 .066.026.13.073.177l6.25 6.25a.25.25 0 0 0 .354 0l5.025-5.025a.25.25 0 0 0 0-.354l-6.25-6.25a.25.25 0 0 0-.177-.073H2.75a.25.25 0 0 0-.25.25ZM6 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2Z"></path>
+        </svg>
+        ${stats.tag}`;
+        });
 
-        if (starsContainer) {
-            starsContainer.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
-                <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"/>
-            </svg>
-            ${stats.stars}`;
-        }
+        starsContainers.forEach(container => {
+            container.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
+            <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"/>
+        </svg>
+        ${stats.stars}`;
+        });
 
-        if (forksContainer) {
-            forksContainer.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
-                <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"/>
-            </svg>
-            ${stats.forks}`;
-        }
+        forksContainers.forEach(container => {
+            container.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" fill="currentColor">
+            <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"/>
+        </svg>
+        ${stats.forks}`;
+        });
     },
 
     init(owner, repo) {
