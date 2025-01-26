6317d634b3ffbf334c3240b9610eadf699f27321
qianmoQ <shicheng@devlive.org>
2025-01-26 11:28:43 +0800

    修复语言标签固定在右上角问题

--- templates/components/block-code.js
+++ templates/components/block-code.js
@@ -1,10 +1,12 @@
 module.exports = function template(item) {
-    const wrapperClass = "!block !w-full !bg-gray-100 !dark:bg-gray-800 !dark:text-gray-300 !text-gray-900 !my-3 !rounded !font-mono !leading-normal !overflow-x-auto !relative";
+    const wrapperClass = "!block !w-full !bg-gray-100 !dark:bg-gray-800 !dark:text-gray-300 !text-gray-900 !my-3 !rounded !font-mono !leading-normal !overflow-hidden !relative";
     const language = item.language ? `language-${item.language}` : '';
-    const languageLabel = item.language ? `<div class="!absolute !right-2 !top-2 !text-sm !text-gray-500 !dark:text-gray-400">${item.language}</div>` : '';
+    const languageLabel = item.language ? `<div class="!absolute !right-2 !top-2 !text-sm !text-gray-500 !dark:text-gray-400 !bg-gray-100 !dark:bg-gray-800 !z-10">${item.language}</div>` : '';
 
     return `<div class="${wrapperClass}">
-       ${languageLabel}
-       <pre class="!px-3 !py-2.5 !m-0"><code class="${language}">${item.text}</code></pre>
-   </div>`;
+        ${languageLabel}
+        <div class="!overflow-x-auto">
+            <pre class="!px-3 !py-2.5 !m-0"><code class="${language}">${item.text}</code></pre>
+        </div>
+    </div>`;
 };
