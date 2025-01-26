0669a143611b2267ec2c8685c3f6b271b29ecaa3
qianmoQ <shicheng@devlive.org>
2025-01-26 12:00:16 +0800

    支持配置显示行号

---
+++ docs/content/usage/code.md
@@ -0,0 +1,37 @@
+---
+title: CodeBlock
+icon: code
+---
+
+PageForge 支持使用代码块，用户可以自定义代码块的样式，以便更好地展示代码的语法和格式。
+
+## 基本语法
+
+---
+
+以下是一个简单的示例：
+
+```java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+```
+
+## 配置行号
+
+---
+
+> 默认情况下，代码块不会显示行号，但可以通过配置 `showLineNumbers` 或者 `showLineNumbers=true` 参数来启用行号显示。格式为 
+> **```java showLineNumbers** 
+> 或者
+> **```java showLineNumbers=true**
+
+```java showLineNumbers
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+```
--- templates/components/block-code.js
+++ templates/components/block-code.js
@@ -3,10 +3,17 @@ module.exports = function template(item) {
     const language = item.language ? `language-${item.language}` : '';
     const languageLabel = item.language ? `<div class="!absolute !right-2 !top-2 !text-sm !text-gray-500 !dark:text-gray-400 !bg-gray-100 !dark:bg-gray-800 !z-10">${item.language}</div>` : '';
 
+    const lines = item.text.split('\n');
+    const lineNumbers = lines.map((_, i) => `<span class="!block !w-8 !pr-2 !text-right !text-gray-500 !select-none">${i + 1}</span>`).join('');
+    const lineNumbersDiv = item.showLineNumbers ? `<div class="!py-2.5 !bg-gray-200 !dark:bg-gray-700">${lineNumbers}</div>` : '';
+
     return `<div class="${wrapperClass}">
         ${languageLabel}
-        <div class="!overflow-x-auto">
-            <pre class="!px-3 !py-2.5 !m-0"><code class="${language}">${item.text}</code></pre>
+        <div class="!overflow-x-auto !flex">
+            ${lineNumbersDiv}
+            <div class="!flex-1">
+                <pre class="!px-3 !py-2.5 !m-0"><code class="${language}">${item.text}</code></pre>
+            </div>
         </div>
     </div>`;
 };
