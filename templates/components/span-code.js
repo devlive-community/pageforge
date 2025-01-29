cf074609e4bc17790581e643a6ea09aac2013961
qianmoQ <shicheng@devlive.org>
2025-01-29 09:06:27 +0800

    修复代码块内容过长导致拉伸宽度

--- templates/components/span-code.js
+++ templates/components/span-code.js
@@ -1,5 +1,5 @@
 module.exports = function template(item) {
     return `
-        <code class="bg-gray-100 text-gray-900 px-1.5 py-0.5 rounded text-sm font-mono mx-1 inline break-words whitespace-pre-wrap">${item.text}</code>
+        <code class="bg-gray-100 text-gray-900 px-1.5 py-0.5 rounded text-sm font-mono mx-1 inline-block max-w-full break-all whitespace-pre-wrap overflow-wrap-anywhere">${item.text}</code>
     `;
 };
