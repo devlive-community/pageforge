472490be812d441a98194084c0685db967d2bf9b
qianmoQ <shicheng@devlive.org>
2025-02-04 19:47:05 +0800

    添加图标加载视觉

--- templates/components/icon.js
+++ templates/components/icon.js
@@ -3,11 +3,16 @@ module.exports = function template(item) {
     const iconName = item.iconName.toLowerCase();
 
     // 默认的样式类
-    const baseStyles = "inline-flex items-center justify-center";
+    const baseStyles = "inline-flex items-center justify-center relative top-[3px]";
+
+    // 骨架屏样式 - 支持明暗模式
+    const skeletonStyles = "absolute inset-0 bg-gray-200 dark:bg-gray-700 animate-pulse rounded";
 
     return `
-        <span class="${baseStyles}">
+        <span class="${baseStyles}" style="width: ${item.size}px; height: ${item.size}px;">
+            <span class="${skeletonStyles}"></span>
             <i data-lucide="${iconName}"
+                class="relative"
                 style="width: ${item.size}px; height: ${item.size}px; color: ${item.color};">
             </i>
         </span>
