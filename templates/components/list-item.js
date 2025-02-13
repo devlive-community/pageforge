5e3e8713acc48119b1b20c25183fd18fb4c481ea
qianmoQ <shicheng@devlive.org>
2025-02-13 09:52:56 +0800

    修复有序列表无法解析问题

--- templates/components/list-item.js
+++ templates/components/list-item.js
@@ -1,25 +1,14 @@
 const {loadComponent} = require('../../lib/component-loader');
 
 module.exports = function template(item) {
-    let listItemContent;
-
     if (item.task) {
-        // 处理任务列表项
         const text = item.text.replace(/\[([\sx])\]/g, '').trim();
         const checkbox = loadComponent('checkbox', {
             checked: item.checked,
             text: text
         });
-        listItemContent = `<li class="mb-2 flex items-start">${checkbox}</li>`;
-    }
-    else {
-        // 处理普通列表项
-        listItemContent = `
-            <li class="mb-2 flex gap-2 text-gray-700 leading-relaxed hover:text-gray-900">
-                <span class="text-gray-600 text-xl select-none w-4">•</span>
-                <span class="flex-1 w-[calc(100%-1rem)]">${item.text}</span>
-            </li>`;
+        return `<li class="mb-2 flex items-start">${checkbox}</li>`;
     }
 
-    return listItemContent;
+    return `<li class="mb-2 text-gray-700">${item.text}</li>`;
 };
